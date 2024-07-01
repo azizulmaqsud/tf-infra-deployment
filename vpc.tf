3dbb2c98b926d17cddab01b8b470df5b47221c5a
Azizul Maqsud <81431470+azizulmaqsud@users.noreply.github.com>
2024-07-01 11:32:38 -0400

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "june-24-infra"
+  name = "july-24-infra"
   cidr = "10.10.0.0/16"
 
-  azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
   public_subnets  = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
 
